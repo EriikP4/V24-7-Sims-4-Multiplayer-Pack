86f99df99a715a896a88ee6753ac2d190925f9ad
Erik <erikperalespozo10@icloud.com>
2024-09-04 22:29:25 +0200

    Updating files

--- first_init/woohoo_raw.bat
+++ first_init/woohoo_raw.bat
@@ -3,7 +3,7 @@
 ::fBE1pAF6MU+EWHreyHcjLQlHcCGQNWCGIrof/eX+4f6UnngtcMMLXb3nwL2eHLcJ6wvhbZNN
 ::fBE1pAF6MU+EWHreyHcjLQlHcCGQNWCGIrof/eX+4f6UnngtcMMLXb3nwL2eHPcU71GqcI4otg==
 ::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFD5GRQ++GGS5E7gZ5vzo09+3hmwpcdADT4DU8ruBbukQ5SU=
+::fBw5plQjdCyDJGyX8VAjFD5GRQ++GGS5E7gZ5vzo082OtmUIVt4JKtuWjfm9Ke0ApxGpWIMhwnNKlMoVCQkVThOoZTMyoHxRoluMOc6IjzjuRWCG6QU5GGoU
 ::YAwzuBVtJxjWCl3EqQJgSA==
 ::ZR4luwNxJguZRRnk
 ::Yhs/ulQjdF+5
@@ -16,24 +16,24 @@
 ::dAsiuh18IRvcCxnZtBJQ
 ::cRYluBh/LU+EWAnk
 ::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJhZksaHErQXA==
-::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbCXrZg==
-::ZQ05rAF9IAHYFVzEqQIGek8ZG0SxNWapRoAM7Onv6vjn
-::eg0/rx1wNQPfEVWB+kM9LVsJDBSDNm6oCaZR/fj/7v6CsC0=
-::fBEirQZwNQPfEVWB+kM9LVsJDBONM2O1CdU=
-::cRolqwZ3JBvQF1fEqQIRKw9BTQiLJmq+Cadc5+7y7OOGrg0ZUaISd4vImrCLYNZBvgqzFQ==
+::cxY6rQJ7JhzQF1fEqQJhZkkaH0rTXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlDMbiXpZg==
+::ZQ05rAF9IAHYFVzEqQIHJxR8QwvCEWq0B7IZ+og=
+::eg0/rx1wNQPfEVWB+kM9LVsJDBONM2O1CYoJ+Oz6+++Vwg==
+::fBEirQZwNQPfEVWB+kM9LVsJDBLQaDyFC7oY+On45NWSskkcQOctGA==
+::cRolqwZ3JBvQF1fEqQIRKw9BTQiLJmq+Cadc7O274uWDsQ0SUus8cY7XmrCLYNZeuBGrIvY=
 ::dhA7uBVwLU+EWHGL4Ec8aC1dTw2DOGSpZg==
 ::YQ03rBFzNR3SWATElA==
 ::dhAmsQZ3MwfNWATElA==
 ::ZQ0/vhVqMQ3MEVWAtB9wSA==
 ::Zg8zqx1/OA3MEVWAtB9wSA==
 ::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFD5GRQ++GGS5E7gZ5vzo09+3hmwpcdADb4DU0ruBH/UD7kTwcIRj02Jf+A==
 ::YB416Ek+Zm8=
 ::
 ::
 ::978f952a14a936cc963da21a135fa983
 @echo off
+chcp 65001>NUL
 IF EXIST "UPDATER_DATA" (
     GOTO UPDATER
 ) ELSE (
@@ -50,13 +50,20 @@ if %ErrorLevel%==1 goto FIRST_TIME
 if %ErrorLevel%==2 goto PWN
 :FIRST_TIME
 cls
-wget --quiet --no-check-certificate "warning url here" -O warning.vbs
+wget --quiet --no-check-certificate "https://raw.githubusercontent.com/EriikP4/WooHoo-Updater/main/first_init/warning.vbs" -O warning.vbs
 start /wait warning.vbs
 echo Descargando librerías...
-wget --quiet --no-check-certificate "full pack url here" -O fullpack.7z
+wget --quiet --no-check-certificate "https://www.udrop.com/file/MhB2/Full_Pack.7z" -O fullpack.7z
+wget --quiet --no-check-certificate "https://www.udrop.com/file/MhBa/Update_v1.7z" -O update1.7z
+wget --quiet --no-check-certificate "https://www.udrop.com/file/MhBb/Update_v2.7z" -O update2.7z
 echo Descomprimiendo librerías...
 7za.exe x -y fullpack.7z
+7za.exe x -y update1.7z
+7za.exe x -y update2.7z
+echo v2 > "%USERPROFILE%\Electronic Arts\Los Sims 4\mod_version.txt"
 del fullpack.7z
+del update1.7z
+del update2.7z
 goto PWN
 :PWN
 cls
@@ -66,4 +73,7 @@ goto UPDATER
 cls
 echo Obteniendo versiones...
 wget --quiet --no-check-certificate "updater url here" -O updater.bat
-start updater.bat && exit
+wget --quiet --no-check-certificate "version url here" -O version.txt
+echo Actualizador iniciado
+start /wait updater.exe
+exit
---
+++ updater/updater_raw.bat
@@ -0,0 +1,58 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+chcp 65001>NUL
+setlocal enabledelayedexpansion
+set version_path="%USERPROFILE%\Documents\Electronic Arts\Los Sims 4\mod_version.txt"
+set /p version=<%version_path%
+set /p cloud=version.txt
+REM NO UPDATES
+if "%version%"=="v2" (
+    echo Estás al día.
+    echo Pulsa cualquier tecla para salir...
+    pause >nul
+    exit
+)
+
+REM V2 -> latest
+if "%version%"=="v2" (
+    set update_link=
+    set update_name=
+    pause >nul
+    exit
+)
+echo Versión actual: %version%
+echo Versión en la nube: %cloud%
+echo ---
+echo Pulsa cualquier tecla para actualizar...
+pause
