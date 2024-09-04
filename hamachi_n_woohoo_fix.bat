5fa69ae347b503ba8ada2137a49c0ca310de6c68
Erik <50298335+EriikP4@users.noreply.github.com>
2024-09-04 19:18:52 +0200

    Código actualizado y mejorado
    Eliminado el privilegio de admin para evitar saltar el antivirus

---
+++ hamachi_n_woohoo_fix.bat
@@ -0,0 +1,14 @@
+chcp 65001>NUL
+mode con:cols=30 lines=10
+@echo off
+echo Añadiendo regla de Firewall para Hamachi...
+netsh advfirewall firewall add rule name="HAMACHI FIX" dir=out program="C:\Program Files (x86)\LogMeIn Hamachi\hamachi-2-ui.exe" profile=any action=allow
+netsh advfirewall firewall add rule name="HAMACHI FIX" dir=in program="C:\Program Files (x86)\LogMeIn Hamachi\hamachi-2-ui.exe" profile=any action=allow
+netsh advfirewall firewall add rule name="HAMACHI FIX" dir=out program="C:\Program Files (x86)\LogMeIn Hamachi\LMIGuardianSvc.exe" profile=any action=allow
+netsh advfirewall firewall add rule name="HAMACHI FIX" dir=in program="C:\Program Files (x86)\LogMeIn Hamachi\LMIGuardianSvc.exe" profile=any action=allow
+netsh advfirewall firewall add rule name="HAMACHI FIX" dir=out program="C:\Program Files (x86)\LogMeIn Hamachi\x64\hamachi-2.exe" profile=any action=allow
+netsh advfirewall firewall add rule name="HAMACHI FIX" dir=in program="C:\Program Files (x86)\LogMeIn Hamachi\x64\hamachi-2.exe" profile=any action=allow
+netsh advfirewall firewall add rule name="HAMACHI FIX" dir=out program="C:\Program Files (x86)\LogMeIn Hamachi\x64\LMIGuardianSvc.exe" profile=any action=allow
+netsh advfirewall firewall add rule name="HAMACHI FIX" dir=in program="C:\Program Files (x86)\LogMeIn Hamachi\x64\LMIGuardianSvc.exe" profile=any action=allow
+echo Añadiendo excepción en Windows Defender
+powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '%USERPROFILE%\Documents\Electronic Arts'"
