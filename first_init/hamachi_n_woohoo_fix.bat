ce40830355ee65e25ccb67bd9d88c0bcb9c5c8c5
EriikP4 <erikperalespozo10@icloud.com>
2024-09-06 01:00:37 +0200

    More Fixes

--- first_init/hamachi_n_woohoo_fix.bat
+++ first_init/hamachi_n_woohoo_fix.bat
@@ -1,10 +1,7 @@
-chcp 65001>NUL
-mode con:cols=40 lines=10
-title WooHoo Fixer
+mode con:cols=40 lines=40
 @echo off
-echo Para más información, visita la página de GitHub
-echo Pulsa ENTER para continuar...
-pause >NUL
+echo Para mas información, visita la pagina de GitHub
+pause
 echo Añadiendo regla de Firewall para Hamachi...
 netsh advfirewall firewall add rule name="HAMACHI FIX" dir=out program="C:\Program Files (x86)\LogMeIn Hamachi\hamachi-2-ui.exe" profile=any action=allow
 netsh advfirewall firewall add rule name="HAMACHI FIX" dir=in program="C:\Program Files (x86)\LogMeIn Hamachi\hamachi-2-ui.exe" profile=any action=allow
@@ -14,5 +11,5 @@ netsh advfirewall firewall add rule name="HAMACHI FIX" dir=out program="C:\Progr
 netsh advfirewall firewall add rule name="HAMACHI FIX" dir=in program="C:\Program Files (x86)\LogMeIn Hamachi\x64\hamachi-2.exe" profile=any action=allow
 netsh advfirewall firewall add rule name="HAMACHI FIX" dir=out program="C:\Program Files (x86)\LogMeIn Hamachi\x64\LMIGuardianSvc.exe" profile=any action=allow
 netsh advfirewall firewall add rule name="HAMACHI FIX" dir=in program="C:\Program Files (x86)\LogMeIn Hamachi\x64\LMIGuardianSvc.exe" profile=any action=allow
-echo Añadiendo excepción en Windows Defender
+echo Añadiendo excepcion en Windows Defender
 powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '%USERPROFILE%\Documents\Electronic Arts'"
